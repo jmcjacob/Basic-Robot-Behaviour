e84dc127cb0407854e796b22db09f598b6832fba
Jacob Carse <jacob.carse@gmail.com>
2016-03-03 20:54:21 +0000

    Structured and partly commented

--- love.py
+++ love.py
@@ -1,3 +1,4 @@
+# Imports all the bits that are needed by the program.
 import rospy
 import cv2
 import numpy
@@ -7,23 +8,27 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
-wheel_radius = 1
-robot_radius = 1
-
 class behave:
 
+    # Constructor for the behave class where vairables are set up for the class such as Publishers and Subscribers.
     def __init__(self):
+        # Creates the window for the image to be displayed in.
         cv2.namedWindow("Image window", 1)
         cv2.startWindowThread()
-        self.threshold = 30
         self.bridge = CvBridge()
+        
+        # Sets up the publisher where a Twist message shall be published from the node.
         self.pub = rospy.Publisher('/turtlebot_1/cmd_vel', Twist, queue_size=10)
+        
+        # Sets up the subscribers to retreive data from the robots laserscanner and rgb camera.
         self.laser_sub = rospy.Subscriber("/turtlebot_1/scan", LaserScan, self.lasercallback)
         self.image_sub = rospy.Subscriber("/turtlebot_1/camera/rgb/image_raw", Image, self.callback)
-                                          
+                     
+    # Callback for the laser that sets the laser data to a vairable.                     
     def lasercallback(self, data):
         self.laser = data                                    
-                                          
+                        
+    # Callback for 
     def callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -41,23 +46,17 @@ class behave:
         l_wheel = 0
         r_wheel = 0
         hit = False
+        twist_msg = Twist()  
         
         ranges = self.laser.ranges
         middle = int(round((len(ranges)-1)/2))
         center = ranges[middle]
         leftRanges = ranges[middle-15:middle]
         rightRanges = ranges[middle:middle+15]
-    
-        if min(leftRanges) < 0.9 or min(rightRanges) < 0.9:
+          
+        if min(ranges) < 0.8:
             print "Too Close"
-            twist_msg = Twist()
-            twist_msg.linear.x = 0.0
-            twist_msg.linear.y = 0.0
-            twist_msg.linear.z = 0.0
-            twist_msg.angular.x = 0.0
-            twist_msg.angular.y = 0.0
             twist_msg.angular.z = 0.2
-            self.pub.publish(twist_msg)
                 
         else:
             leftRange = numpy.zeros((height,width,1), numpy.uint8)
@@ -67,7 +66,7 @@ class behave:
 
             if cv2.countNonZero(leftRange) > 0:
                 print "left"
-                if mean > self.threshold:
+                if mean > 30:
                     l_wheel = 0.5 # Love
                 else:
                     r_wheel = 0.5 # Fear
@@ -75,63 +74,67 @@ class behave:
             
             if cv2.countNonZero(rightRange) > 0:
                 print "right"
-                if mean > self.threshold:
+                if mean > 30:
                     r_wheel = 0.5 # Love
                 else:
                     l_wheel = 0.5 # Fear
                 hit = True
             
-        if hit:
-            twist_msg = Twist()
-            if r_wheel == l_wheel:
-                print "center"
-                if math.isnan(center):
-                    value = 0.5
+            # Makes the robot approtch the target.
+            if hit:
+                # If both values are equal the target is centered.
+                if r_wheel == l_wheel:
+                    print "center"
+                    if math.isnan(center):                        # Checks if center range is not NaN.
+                        value = 0.5
+                    else:
+                        value = 1.0 - (1.0/center)                # Calculates wheel values for robot to approtch target.
+                        if value <= 0:
+                            value = 0
+                    (v, a) = forward_kinematics(value, value)     # Calculates the velocity based on range to the target.
+                    
                 else:
-                    value = 1.0 - (1.0/center)
-                    if value <= 0:
-                        value = 0
-                (v, a) = forward_kinematics(value, value)
+                    (v, a) = forward_kinematics(l_wheel, r_wheel) # Calcualtes velocity based on wheel values
+                    
+                # Sets velocity to message
                 twist_msg.linear.x = v
                 twist_msg.angular.z = a
+            
+            # Makes the robot explore/
             else:
-                (v, a) = forward_kinematics(l_wheel, r_wheel)
-                twist_msg.linear.x = v
-                twist_msg.angular.z = a
-            self.pub.publish(twist_msg)
+                # Makes the robot explore forwards when nothing is obstructing it.
+                if min(leftRanges) > 2.0 and min(rightRanges) > 2.0:
+                    print "explore forward"
+                    twist_msg.linear.x = 0.5
+                    twist_msg.angular.z = 0.0
+                # Turns the robot left when something is on the right of the robot.
+                elif min(leftRanges) > 2.0 and min(rightRanges) < 2.0:
+                    print "explore left"
+                    twist_msg.linear.x = 0.0
+                    twist_msg.angular.z = -0.5
+                # Turns the robot right when something is on the left.
+                elif min(leftRanges) < 2.0 and min(rightRanges) > 2.0:
+                    print "explore right"
+                    twist_msg.linear.x = 0.0
+                    twist_msg.angular.z = 0.5
+                # If the robot is unsure about ranges will spin until sure.
+                else:
+                    print "explore recover"
+                    twist_msg.linear.x = 0.0
+                    twist_msg.angular.z = 0.5
             
-        else:
-            twist_msg = Twist()
-            
-            if min(leftRanges) > 2.0 and min(rightRanges) > 2.0:
-                print "explore forward"
-                twist_msg.linear.x = 0.5
-                twist_msg.angular.z = 0.0
-            elif min(leftRanges) > 2.0 and min(rightRanges) < 2.0:
-                print "explore left"
-                twist_msg.linear.x = 0.0
-                twist_msg.angular.z = -0.5
-            elif min(leftRanges) < 2.0 and min(rightRanges) > 2.0:
-                print "explore right"
-                twist_msg.linear.x = 0.0
-                twist_msg.angular.z = 0.5
-            else:
-                print "explore recover"
-                twist_msg.linear.x = 0.0
-                twist_msg.angular.z = 0.5
-            self.pub.publish(twist_msg)
-            
-        cv2.imshow("Image window", cv_image)
+        self.pub.publish(twist_msg)           # Publishes the the Twist Message to cmd_vel.
+        cv2.imshow("Image window", cv_image)  # Displays cv_image to the image window.
 
-            
+# Fucnction for converting wheel speeds into a linear and angular velocity. (Hanhide, 2016)
 def forward_kinematics(w_l, w_r):
-    c_l = wheel_radius * w_l
-    c_r = wheel_radius * w_r
+    c_l = 1 * w_l
+    c_r = 1 * w_r
     v = (c_l + c_r) / 2
-    a = (c_l - c_r) / robot_radius
+    a = (c_l - c_r) / 1
     return (v, a)
 
-rospy.init_node('node', anonymous=True)
-behave()
-rospy.spin()
-cv2.destroyAllWindows()
+rospy.init_node('node', anonymous=True)       # Initiates the Ros node that will publish topics from.
+behave()                                  # Runs the class to control the robot.
+rospy.spin()                              # Make the application run until exited.
+cv2.destroyAllWindows()                   # Closes all windows opened by OpenCV
